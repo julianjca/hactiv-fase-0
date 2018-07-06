fb1510976eb757ef509fdd7f426359b1a2dc41d9
julianjca <julianjca11@gmail.com>
2018-07-06 21:45:02 +0700

    Push if-else.js

---
+++ if-else/if-else.js
@@ -0,0 +1,19 @@
+var nama = 'Julian';
+var peran = 'Ksatria';
+
+if(nama === ''){
+  console.log('Nama harus diisi!');
+} if(peran === ''){
+  console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
+} if(nama!==''&&peran === 'Ksatria'){
+  console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+  console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`)
+}
+if(nama!==''&&peran === 'Tabib'){
+  console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+  console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`)
+}
+if(nama!==''&&peran === 'Penyihir'){
+  console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+  console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
+}

12834498bba22ea995bb111fc5dd4f7c09b0c473
julianjca <julianjca11@gmail.com>
2018-07-10 10:52:29 +0700

    More Javascript Case

--- perkalianunik.js
+++ perkalianunik.js
@@ -5,12 +5,16 @@ function perkalianunik(arr){
     for(let j = 0; j<arr.length;j++){
       if(i!==j){
         simpan  = simpan*arr[j];
-        newArray[i]= simpan;
+        //newArray[i]= simpan;
       }
     }
+    newArray.push(simpan);
     simpan = 1;
   }
   return newArray;
 }
 
+console.log(perkalianunik([2,4,6]));
 console.log(perkalianunik([1,2,3,4,5]));
+console.log(perkalianunik([1,3,3,1]));
+console.log(perkalianunik([2,1,8,10,2]));

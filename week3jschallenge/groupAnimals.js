06cfe0ef28c69ff8a03c31eaeb4eee7e9e633fb4
julianjca <julianjca11@gmail.com>
2018-07-11 12:47:19 +0700

    add group animals

---
+++ week3jschallenge/groupAnimals.js
@@ -0,0 +1,27 @@
+function groupAnimals(animals) {
+  let result = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let counter = 0;
+  while(counter<alphabet.length){
+    let sementara =[];
+    let ada = 0;
+    for(let i = 0; i< animals.length;i++){
+      if(animals[i][0]===alphabet[counter]){
+        sementara.push(animals[i]);
+        ada = 1;
+      }
+    }
+    if(ada===1){
+      result.push(sementara);
+    }
+    counter++;
+  }
+
+  return result;
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
